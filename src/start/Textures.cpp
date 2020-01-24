630efc3b30f5dd50d3ed297cdc512151b87a8208
zsh <1026860069@qq.com>
2020-01-24 13:08:00 +0800

    正确使用stb_image.h

--- src/start/Textures.cpp
+++ src/start/Textures.cpp
@@ -1,7 +1,6 @@
 // 纹理
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#define STB_IMAGE_IMPLEMENTATION // 预处理器会修改头文件，让其只包含相关的函数定义源码，等于是将这个头文件变为一个 .cpp 文件了
 #include <learnopengl/shader_s.h>
 #include <stb_image.h> // 图片加载器 三方
 #include <iostream>
